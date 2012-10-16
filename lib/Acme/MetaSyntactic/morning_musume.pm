d13095fca30032e919421d8b1d94aa6e7b16dd51
Laurent Boivin <elbeho@cpan.org>
2012-10-16 23:33:46 +0000

    Import of ELBEHO/Acme-MetaSyntactic-morning_musume-1.001 from CPAN.
    gitpan-cpan-distribution: Acme-MetaSyntactic-morning_musume
    gitpan-cpan-version:      1.001
    gitpan-cpan-path:         ELBEHO/Acme-MetaSyntactic-morning_musume-1.001.tar.gz
    gitpan-cpan-author:       ELBEHO
    gitpan-cpan-maturity:     released

--- lib/Acme/MetaSyntactic/morning_musume.pm
+++ lib/Acme/MetaSyntactic/morning_musume.pm
@@ -3,7 +3,7 @@ use strict;
 use Acme::MetaSyntactic::MultiList;
 use Acme::MorningMusume;
 our @ISA     = qw( Acme::MetaSyntactic::MultiList );
-our $VERSION = '1.000';
+our $VERSION = '1.001';
 
 	my $musume = Acme::MorningMusume->new;
 	#my @all      = map { $_->name_en } $musume->members;
@@ -23,15 +23,35 @@ __PACKAGE__->init(
 
 =head1 NAME
 
-Acme::MetaSyntactic::morning_musume
+Acme::MetaSyntactic::morning_musume -- The theme of Morning Musume
 
 =head1 DESCRIPTION
 
-Think about around Morning Musume
+The names (english transcription) of the Japanese Pop Idols Morning Musume
+
+The list is provided by the module Acme::MorningMusume from KENTARO
+
+Two themes are available: active (default), graduate
+
+=head1 INSTALL
+
+To install this module, run the following commands:
+
+    perl Build.PL
+    ./Build
+    ./Build test
+    ./Build install
+
+Alternatively, to install with ExtUtils::MakeMaker, you can use the following commands:
+
+    perl Makefile.PL
+    make
+    make test
+    make install
 
 =head1 CONTRIBUTOR
 
-Laurent Boivin
+Laurent Boivin (elbeho)
 
 =head1 CHANGES
 
@@ -39,9 +59,15 @@ Laurent Boivin
 
 =item *
 
+2012-10-17 - v1.001
+
+Fix documentation (pod and readme)
+
+=item *
+
 2012-10-13 - v1.000
 
-First attempt... don't be rude. Gomen nasai.
+First version of my first module
 
 =back
 
@@ -50,11 +76,14 @@ First attempt... don't be rude. Gomen nasai.
 L<Acme::MorningMusume>,
 L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
 
+=head1 COPYRIGHT
+
+Copyright (C) 2012 Laurent Boivin (elbeho)
+
 =head1 LICENSE
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
 =cut
-
 
